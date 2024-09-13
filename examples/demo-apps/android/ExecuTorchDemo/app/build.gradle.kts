d3b2d77bae595039f61773e5a5b4de29d4fca969
Hansong Zhang <hsz@fb.com>
2024-09-13 14:28:16 -0700

    IC4 repro

--- examples/demo-apps/android/ExecuTorchDemo/app/build.gradle.kts
+++ examples/demo-apps/android/ExecuTorchDemo/app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
   implementation("org.pytorch.executorch:executorch") {
     exclude("com.facebook.fbjni", "fbjni-java-only")
   }
+  implementation(files("libs/executorch.aar"))
   testImplementation("junit:junit:4.13.2")
   androidTestImplementation("androidx.test.ext:junit:1.1.5")
   androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
